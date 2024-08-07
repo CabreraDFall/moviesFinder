d4ff25d2b632dc76fc03b81bebf1fe514c313e0a
CabreraDFall <testingdfall@gmail.com>
2024-08-07 07:19:13 +0200

    Menu Principal

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { View, Text } from "react-native";
-
+import { NativeWindStyleSheet } from "nativewind";
 import AppNavigation from "./navigation/appNavigation";
-
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 const App = () => {
   return <AppNavigation />;
 };
--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,13 +1,37 @@
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import {
+  Platform,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { style } from "nativewind";
+import {
+  Bars3CenterLeftIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+import { styles, theme } from "../theme";
+
+const ios = Platform.OS == "ios";
 
 const HomeScreen = () => {
   return (
     <View className="flex-1 bg-neutral-800">
-      <SafeAreaView>
-        <Text className=" text-white text-2xl font-bold">hola</Text>
+      <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
+        <StatusBar style="light" />
+        <View className="flex-row justify-between items-center mx-4">
+          <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+          <Text className="text-white text-2xl font-bold">Movies</Text>
+          <TouchableOpacity>
+            <MagnifyingGlassIcon size="24" strokeWidth={2} color="white" />
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 10 }}
+      ></ScrollView>
     </View>
   );
 };
